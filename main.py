acd8073a48bcef9c954dafffe7690d4b32f0bacc
ShifanAhamed <shifanahamed007@gmail.com>
2025-07-30 20:19:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 from pyrogram import Client, filters
 
-API_ID = int(os.environ.get("28906453"))
-API_HASH = os.environ.get("f494712f1d11956c1954e2cbbd984370")
-BOT_TOKEN = os.environ.get("7746953136:AAER6ehls2fS2ny4zO3wWcvBEcxg_YB_UD4")
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 app = Client("rename_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 

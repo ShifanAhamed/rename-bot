41a39cba216d6527029cc46ee4def3d5f6a43026
ShifanAhamed <shifanahamed007@gmail.com>
2025-07-30 22:03:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import time
 from pyrogram import Client, filters
 
+# ✅ TIME PATCH for "msg_id is too low" bug
 original_time = time.time
 time.time = lambda: int(original_time())
 
@@ -9,7 +10,7 @@ API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-app = Client("rename_bot", api_id=API_ID, api_hash=API_ID, bot_token=BOT_TOKEN)
+app = Client("rename_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
